18502a66e086bb0524be5055bca5a2fb85e35f2e
AmokSingh <amoksmehta@gmail.com>
2026-04-25 10:03:18 +0530

    Update server URL to production endpoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://chat-application-backend-3qg4.onrender.com";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
